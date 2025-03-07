af5e136c2c1d6ae072435bb69b26796da2f72dd3
Vilmar Prestes <prestesvilmar20@gmail.com>
2025-03-07 00:13:28 -0300

    feat: added CORS configs for frontend

--- src/main/java/com/VilmarPrestes/crud_escolar/controllers/AlunoController.java
+++ src/main/java/com/VilmarPrestes/crud_escolar/controllers/AlunoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/alunos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AlunoController {
     private final AlunoService alunoService;
 
